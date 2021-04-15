a135254d0410f8d7d02b45c2f8d079655e3e0dad
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2021-04-14 20:36:07 -0700

    verbosity level

--- ocaml-grammars/arsenal_lib/src/arsenal_options.ml
+++ ocaml-grammars/arsenal_lib/src/arsenal_options.ml
@@ -17,6 +17,7 @@ let howmany = ref 1        (* Default number of example to generate is 1 *)
 let raw_json = ref false   (* Whether json should just be like Sexp *)
 let injectivity = ref false (* Whether to check that the pretty-printing function is injective -- requires saving in memory all instances produced *)
 let no_duplicates = ref false (* Whether to ensure there are no duplicates -- requires saving in memory all instances produced *)
+let verb = ref 0 (* Verbosity level *)
 let args : string list ref = ref [] (* Where the command-line argument will be stacked *)
 
 let options =
@@ -34,6 +35,7 @@ let options =
     ("-check-injectivity", Set injectivity, "\tcheck there are no two ASTs for the same NL (default is false) -- saves generated instances in memory");
     (* ("-check", Set check_opt, "\tcheck output"); *)
     ("-strict-entities", Float(fun f -> Entity.strict := f), "\thow strict entity kinds should be considered (0. : they are ignored; +infty: very strictly enforced)");
+    ("-verb", Int(fun i -> verb := i), "\tverbosity level (default is 0)");
   ]
 
 exception NonInjective of { nl      : string;
@@ -67,6 +69,7 @@ let generate (About.About{ key ; serialise = { to_json ; to_sexp; _} ; _ }) n =
       if !raw_json     then sexp |> sexp2json |> JSON.to_string        |> print_tab;
       if !print_sexp   then sexp |> Sexp.to_string                     |> print_tab;
       print_endline "";
+      if !verb > 0 && !sofar mod 1000 = 0 then Format.(fprintf stderr) "@[%i@]@,%!" !sofar;
       incr sofar;
     in
     if !injectivity || !no_duplicates
