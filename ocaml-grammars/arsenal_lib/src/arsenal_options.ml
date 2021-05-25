c3adbe1af32228b22e6787addd188d2ca13d58bd
Stephane Graham-Lengrand <stephane.graham-lengrand@sri.com>
2021-05-25 11:52:08 -0700

    command line options

--- ocaml-grammars/arsenal_lib/src/arsenal_options.ml
+++ ocaml-grammars/arsenal_lib/src/arsenal_options.ml
@@ -13,30 +13,32 @@ let print_nl   = ref true  (* Output Natural Language by default *)
 let print_json = ref false (* Do not output json by default *)
 let print_sexp = ref false (* Do not output S-expression by default *)
 let print_polish = ref false (* Do not output polish notation by default *)
-let howmany = ref 1        (* Default number of example to generate is 1 *)
-let raw_json = ref false   (* Whether json should just be like Sexp *)
+let pretty     = ref false  (* prettyfy S-expressions *) 
+let howmany    = ref 1      (* Default number of example to generate is 1 *)
+let raw_json   = ref false  (* Whether json should just be like Sexp *)
 let injectivity = ref false (* Whether to check that the pretty-printing function is injective -- requires saving in memory all instances produced *)
 let no_duplicates = ref false (* Whether to ensure there are no duplicates -- requires saving in memory all instances produced *)
 let args : string list ref = ref [] (* Where the command-line argument will be stacked *)
 
 let options =
   [
-    ("-n", Int(fun i -> howmany := i), "\thow many instances to generate (default is 1)");
-    ("-json", Set print_json, "\tprint json output");
-    ("-sexp", Set print_sexp, "\tprint S-expression output");
-    ("-polish", Set print_polish, "\tprint polish output");
-    ("-raw-json", Set raw_json, "\tmaximal Produce raw JSONs (i.e. JSON versions of S-expressions) (default is false)");
     ("-arity_sep", String(fun s -> Polish.arity := s), "\tin Polish notation, specify separator between token and its arity (default is '#')");
-    ("-one-entity", Set Entity.one_entity_kind, "\tin natural language, only use 1 kind of entities");
-    ("-short", Set short, "\tcontructors and entity kinds have short strings and are not prefixed by module names");
-    ("-hide-entities", Clear Entity.ppkind, "\tif a substitution string is present in entity, just show the string");
-    ("-types", Set PPX_Serialise.print_types, "\tdisplay types in generated data");
-    ("-no-nl", Clear print_nl, "\tprint disable natural language output");
-    ("-no-duplicates", Set no_duplicates, "\tensure there are no duplicates (default is false) -- saves generated instances in memory");
+    (* ("-check", Set check_opt, "\tcheck output"); *)
     ("-check-injectivity", Set injectivity, "\tcheck there are no two ASTs for the same NL (default is false) -- saves generated instances in memory");
     ("-deterministic", Set deterministic, "\tDeterministic NL generation (default is false)");
-    (* ("-check", Set check_opt, "\tcheck output"); *)
+    ("-hide-entities", Clear Entity.ppkind, "\tif a substitution string is present in entity, just show the string");
+    ("-json", Set print_json, "\tprint json output");
+    ("-n", Int(fun i -> howmany := i), "\thow many instances to generate (default is 1)");
+    ("-no-duplicates", Set no_duplicates, "\tensure there are no duplicates (default is false) -- saves generated instances in memory");
+    ("-no-nl", Clear print_nl, "\tprint disable natural language output");
+    ("-one-entity", Set Entity.one_entity_kind, "\tin natural language, only use 1 kind of entities");
+    ("-polish", Set print_polish, "\tprint polish output");
+    ("-pretty", Set pretty, "\tprint S-expressions in human-readable form");
+    ("-raw-json", Set raw_json, "\tmaximal Produce raw JSONs (i.e. JSON versions of S-expressions) (default is false)");
+    ("-sexp", Set print_sexp, "\tprint S-expression output");
+    ("-short", Set short, "\tcontructors and entity kinds have short strings and are not prefixed by module names");
     ("-strict-entities", Float(fun f -> Entity.strict := f), "\thow strict entity kinds should be considered (0. : they are ignored; +infty: very strictly enforced)");
+    ("-types", Set PPX_Serialise.print_types, "\tdisplay types in generated data");
     ("-verb", Int(fun i -> verb := i), "\tverbosity level (default is 0)");
   ]
 
@@ -69,7 +71,10 @@ let generate (About.About{ key ; serialise = { to_json ; to_sexp; _} ; _ }) n =
       if !print_json   then t |> to_json |> JSON.to_string     |> print_tab;
       if !print_polish then sexp |> Polish.of_sexp |> Polish.to_string |> print_tab;
       if !raw_json     then sexp |> sexp2json |> JSON.to_string        |> print_tab;
-      if !print_sexp   then sexp |> Sexp.to_string                     |> print_tab;
+      if !print_sexp   then sexp
+                            |> Format.sprintf "@,@[<v>%a@]"
+                                 Sexp.(if !pretty then pp_hum else pp_mach)
+                            |> print_tab;
       print_endline "";
       if !verb > 0 && !sofar mod 1000 = 0 then Format.(fprintf stderr) "@[%i@]@,%!" !sofar;
       incr sofar;
