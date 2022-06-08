95ee5eafbce22b8aeb547e11a1625f22bd466ba1
Karsten Martiny <karsten.martiny@sri.com>
2022-06-08 14:04:27 -0700

    option to emit confidence scores from trained model

--- transformers/runtime/run_nl2cst_transformers.py
+++ transformers/runtime/run_nl2cst_transformers.py
@@ -1,6 +1,7 @@
 import json
 import math
 
+import torch
 from flask import Flask
 from flask import request
 import os, sys
@@ -24,6 +25,13 @@ def get_env(argname, default=None):
             sys.exit("{} needs to be specified as an environment variable, exiting".format(argname))
         return default
 
+def get_bool_opt(var_name : str):
+    if var_name in os.environ and int(get_env(var_name)) is not 0:
+        return True
+    else:
+        return False
+
+
 MODEL_ROOT = get_env('MODEL_ROOT') # the root of the training output
 NL2CST_HOST = get_env('NL2CST_HOST', '127.0.0.1')
 NL2CST_PORT = get_env('NL2CST_PORT', 8080)
@@ -32,6 +40,7 @@ NUM_OUTPUTS = int(get_env("NUM_OUTPUTS"))
 TYPE_FORCING = int(get_env("TYPE_FORCING"))
 BATCH_SIZE = int(get_env("BATCH_SIZE"))
 CLEAN_INPUT = int(get_env("CLEAN_INPUT"))
+INCLUDE_SCORES = get_bool_opt("INCLUDE_SCORES")
 
 app = Flask(__name__)
 
@@ -90,15 +99,18 @@ def process():
 
         input_tokens = source_tokenizer(batch, padding="max_length", truncation=True, return_tensors="pt",
                               max_length=max_input_length)
-        output_tokens = bert2arsenal.generate(
+        generated = bert2arsenal.generate(
             input_ids=input_tokens.input_ids,
             attention_mask=input_tokens.attention_mask,
             decoder_start_token_id=target_tokenizer.cls_token_id,
             num_beams=NUM_BEAMS,
             num_return_sequences=NUM_OUTPUTS,
             type_forcing_vocab=type_forcing_vocab,
-            no_repeat_ngram_size=0  # default was 3, but this punishes desired translations -> figure out if/what setting we want to use here
+            no_repeat_ngram_size=0,  # default was 3, but this punishes desired translations -> figure out if/what setting we want to use here
+            output_scores = True,
+            return_dict_in_generate=True,
         )
+
 
         # iterate over instances in batch to prepare outputs
         for j in range(batch_start, batch_end):
@@ -109,9 +121,59 @@ def process():
                 # if beam search returns multiple outputs per input, those are stacked along the same dimension as the
                 # outputs of different sentences, so we'll need to iterate over the output tensor in intervals of NUM_OUTPUTS
                 csts = []
+                scores = []
                 for k in range(j-batch_start, j-batch_start+NUM_OUTPUTS):
-                    csts.append(target_tokenizer.runtime_decode(output_tokens[k].tolist()))
-                results.append({"id": id, "nl": nl, "cst": csts})
+
+                    '''
+                    the scores are given in a somewhat odd representation:
+                    - each token is scored (including potential trailing padding tokens)
+                    - as usual, the length of the token sequence is determined by the 
+                      longest sequence in the batch
+                    - scores are represented as a list with length of len(token_seq)-1
+                      (because the start token is not scored)
+                    - each entry in the score list has dimension (BATCH_SIZE, VOCAB_SIZE)
+                    To get the scores for a specific instance (with index k), we thus need 
+                    to iterate over the list of scores and then get the kth element of each
+                    list entry 
+                    '''
+                    if INCLUDE_SCORES:
+
+
+                        score_values = generated["scores"]
+
+                        tokens = generated["sequences"][k].tolist()
+
+                        logit_scores = []
+                        softmax_scores = []
+
+                        for idx, values in enumerate(score_values):
+
+                            # tokens are shifted by 1 because the first token isn't scored
+                            # 0 is the padding token - once we encounter the first once
+                            # we should stop collecting scores
+                            if tokens[idx+1] == 0:
+                                break
+
+                            logits = values[k]
+                            softmax = torch.softmax(logits, dim=0)
+                            max_logit = torch.max(logits).item()
+                            max_softmax = torch.max(softmax).item()
+
+                            logit_scores.append(max_logit)
+                            softmax_scores.append((max_softmax))
+
+                        min_logit_score = min(logit_scores)
+                        min_softmax_score = min(softmax_scores)
+
+                        scores.append({"logit_scores": logit_scores, "softmax_scores": softmax_scores,
+                                       "min_logit_score": min_logit_score, "min_softmax_score": min_softmax_score,
+                                       "tokens": tokens })
+                    csts.append(target_tokenizer.runtime_decode(generated["sequences"][k].tolist()))
+
+                result = {"id": id, "nl": nl, "cst": csts}
+                if INCLUDE_SCORES:
+                    result["scores"] = scores
+                results.append(result)
             except Exception as e:
                 print(f"Exception processing sentence: {repr(e)}")
                 results.append({"error": repr(e)})
