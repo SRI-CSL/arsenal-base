d157e67d41737d79e2e199914de706711aa3ca6f
Karsten Martiny <karsten.martiny@sri.com>
2022-06-28 07:05:22 -0700

    remove some project-specific references

--- regexp/generate-reformulate/docker/builder/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker push sricsl/regex-builder:`cat VERSION`
-docker push sricsl/regex-builder:latest
--- regexp/generate-reformulate/docker/reformulator/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker push sricsl/effigy-reformulate:`cat ../../version`
