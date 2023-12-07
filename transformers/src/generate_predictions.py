25415bf4a2ba62a5540e9a49ed12e3a9776d7e32
Karsten Martiny <karsten.martiny@sri.com>
2023-12-07 22:45:43 +0000

    update prediction generation for latest transformers

--- transformers/src/generate_predictions.py
+++ transformers/src/generate_predictions.py
@@ -8,36 +8,13 @@ import datasets
 import torch
 from tabulate import tabulate
 from tqdm import tqdm
-from transformers import EncoderDecoderModel, BertTokenizerFast
+from transformers import EncoderDecoderModel, BertTokenizerFast, GenerationConfig
 from transformers.trainer_utils import get_last_checkpoint
 
 from args import parse_arguments
 from arsenal_tokenizer import PreTrainedArsenalTokenizer
 
 torch.set_printoptions(threshold=5000)
-
-# def str2bool(v):
-#     if isinstance(v, bool):
-#        return v
-#     if v.lower() in ('yes', 'true', 't', 'y', '1'):
-#         return True
-#     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-#         return False
-#     else:
-#         raise argparse.ArgumentTypeError('Boolean value expected.')
-#
-# parser = argparse.ArgumentParser(description="Relation Extraction")
-# parser.add_argument("-data_dir",       type=str,       default="../arsenal/large_files/datasets/2021-07-30T0004", help="location of the data directory")
-# parser.add_argument("-fp16",           type=str2bool,  default=True,                    help="Mixed precision training, can only be used on CUDA devices")
-# parser.add_argument("-model_dir",      type=str,       default="../arsenal/large_files/models/transformers/04-29-2021/translation_model",  help="location of the trained translation model")
-# parser.add_argument("-num_beams",      type=int,       default=1,                       help="number of beams to use in beam search when generating predictions")
-# parser.add_argument("-num_outputs",    type=int,       default=1,                       help="number of beam search outputs to generate for each instance")
-#
-# args = parser.parse_args()
-# data_dir = args.data_dir
-# model_dir = args.model_dir
-# num_beams = args.num_beams
-# num_outputs = args.num_outputs
 
 def generate_predictions(args):
 
@@ -68,13 +45,28 @@ def generate_predictions(args):
 
     type_forcing_vocab = target_tokenizer.id2vocab if args.type_forcing else None
 
+    val_data.set_format(
+        type="torch", 
+        columns=["input_ids", "attention_mask"],
+    )
+
+    generation_config = GenerationConfig(
+        num_beams=args.num_beams,
+        num_return_sequences=args.num_outputs,
+        no_repeat_ngram_size=0,
+        decoder_start_token_id=target_tokenizer.cls_token_id,
+        eos_token_id=bert2arsenal.config.eos_token_id,
+        pad_token_id=bert2arsenal.config.pad_token_id,
+        max_new_tokens=bert2arsenal.config.max_length
+    )
+
     for i in tqdm(range(num_batches)):
 
         batch_range = range(i*batch_size, (i+1)*batch_size)
         batch = val_data.select(list(batch_range))
 
-        batch_ids = torch.tensor(batch["input_ids"], device=torch_device)
-        batch_masks = torch.tensor(batch["attention_mask"], device=torch_device)
+        batch_ids = batch["input_ids"].to(device=torch_device)
+        batch_masks = batch["attention_mask"].to(device=torch_device)
 
         # take this little detour with the args for generate() so that we can decide whether
         # we want to add the argument for the type forcing vocab (if using an unpatched
@@ -83,19 +75,15 @@ def generate_predictions(args):
         generate_args = {
             "input_ids": batch_ids,
             "attention_mask": batch_masks,
-            "decoder_start_token_id": target_tokenizer.cls_token_id,
-            "num_beams": args.num_beams,
-            "num_return_sequences": args.num_outputs,
-            "no_repeat_ngram_size": 0
         }
         if args.type_forcing:
             generate_args["type_forcing_vocab"] = type_forcing_vocab
 
-        outputs = bert2arsenal.generate(**generate_args)
+        outputs = bert2arsenal.generate(input_ids=batch_ids, attention_mask=batch_masks, generation_config=generation_config)
 
         # apparently batch instances and return sequences per instance are stacked along a single dimension
         for j in range(batch_size):
-            input = [t for t in batch["input_ids"][j] if t != 0]
+            input = [t for t in batch["input_ids"][j].tolist() if t != 0]
             true_seq = [t for t in batch['labels'][j] if t != -100]
             outfile.write(f"{input}\t{true_seq}")
             for k in range(j*args.num_outputs, (j+1)*args.num_outputs):
